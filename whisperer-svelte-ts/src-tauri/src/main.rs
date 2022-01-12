f59dcdf5c0f5cf7e4e2ed0d257346486f1efd684
BORBER <30563826+Borber@users.noreply.github.com>
2022-01-12 16:57:18 +0800

    [mod] 添加 whisperer-svelte-ts 第一版, GUI 支持开始

---
+++ whisperer-svelte-ts/src-tauri/src/main.rs
@@ -0,0 +1,40 @@
+#![cfg_attr(
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
+)]
+
+use serde::Deserialize;
+use tauri::command;
+use whisperer::{encode, decode, config::Conf};
+
+#[derive(Debug, Deserialize)]
+pub struct RequestBody {
+    s: String
+}
+
+#[command]
+fn encode_api(endpoint: String, body: RequestBody) -> String {
+    println!("{} {:?}", endpoint, body);
+    format!("{}{}", Conf::global().flag, encode(body.s))
+}
+
+#[command]
+fn decode_api(endpoint: String, body: RequestBody) -> String {
+    println!("{} {:?}", endpoint, body);
+    if body.s.starts_with(&Conf::global().flag) {
+        decode(body.s.replace(&Conf::global().flag, ""))
+    } else {
+        format!("请检查你的输入格式, 形如 -> {}XXX", &Conf::global().flag)
+    }
+}
+
+fn main() {
+    Conf::init_conf(r"C:\Users\BORBER\Code\Rust\Whisperer\whisperer\config.toml");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            encode_api,
+            decode_api
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ whisperer-svelte-ts/src/App.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import {invoke} from "@tauri-apps/api/tauri";
+
+    let encode_text;
+    let result = "";
+    const en = () => {
+        invoke("encode_api", {
+            endpoint: "加密",
+            body: {
+                s: (<HTMLInputElement> encode_text).value
+            },
+        }).then(
+            (res) => {
+                result = res.toString()
+            }
+        )
+    };
+    const de = () => {
+        invoke("decode_api", {
+            endpoint: "解密",
+            body: {
+                s: (<HTMLInputElement> encode_text).value
+            },
+        }).then(
+            (res) => {
+                result = res.toString()
+            }
+        )
+    };
+</script>
+
+<textarea bind:this={encode_text}></textarea>
+
+<button on:click={en}>
+    加密
+</button>
+<button on:click={de}>
+    解密
+</button>
+
+<p>{result}</p>
+
+<style>
+</style>
