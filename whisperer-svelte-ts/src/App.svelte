01560f86c0236d053faf6ea76f54e9d9b4fcd7a8
BORBER <borber.drum@gmail.com>
2022-01-19 21:36:06 +0800

    [mod] 美化 svelte-ts GUI

--- whisperer-svelte-ts/src/App.svelte
+++ whisperer-svelte-ts/src/App.svelte
@@ -2,16 +2,18 @@
     import {invoke} from "@tauri-apps/api/tauri";
 
     let encode_text;
+    let decode_text;
     let result = "";
     const en = () => {
         invoke("encode_api", {
             endpoint: "加密",
             body: {
-                s: (<HTMLInputElement> encode_text).value
+                s: encode_text.value
             },
         }).then(
             (res) => {
                 result = res.toString()
+                decode_text.value = result
             }
         )
     };
@@ -19,11 +21,12 @@
         invoke("decode_api", {
             endpoint: "解密",
             body: {
-                s: (<HTMLInputElement>encode_text).value
+                s: decode_text.value
             },
         }).then(
             (res) => {
                 result = res.toString()
+                encode_text.value = result
             }
         )
     };
@@ -32,20 +35,87 @@
     }
 </script>
 
-<textarea bind:this={encode_text}></textarea>
+<textarea bind:this={encode_text} id="et" placeholder="明文"></textarea>
 
-<button on:click={en}>
-    加密
-</button>
-<button on:click={de}>
-    解密
-</button>
+<div class="buttons">
+    <button id="eb" on:click={en}>
+        ▶
+    </button>
+    <button id="db" on:click={de}>
+        ◀
+    </button>
+    <button id="copy" on:click={cp}>
+        𝑪𝑶𝑷𝒀
+    </button>
+</div>
 
-<p>{result}</p>
-
-<button on:click={cp}>
-    复制
-</button>
+<textarea bind:this={decode_text} id="dt" placeholder="密文"></textarea>
 
 <style>
+    :global(body) {
+        margin: 0;
+    }
+
+    :global(#app) {
+        display: flex;
+    }
+
+    textarea {
+        width: 262px;
+        height: 320px;
+        border: none;
+        box-sizing: border-box;
+        color: #a7a9be;
+        background: #0f0e17;
+        padding: 0.5em;
+        font-size: 1em;
+        font-family: "FOT-ModeMinALarge Std", serif;
+        resize: none;
+    }
+
+    textarea:focus {
+        outline: none;
+    }
+
+    textarea::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+    }
+
+    textarea::-webkit-scrollbar-thumb {
+        border-radius: 3px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        background-color: #c3c3c3;
+    }
+
+    textarea::-webkit-scrollbar-track {
+        background-color: transparent;
+    }
+
+    button {
+        padding: 0;
+        border: none;
+        color: #fffffe;
+        width: 76px;
+        height: 106px;
+    }
+
+    .buttons {
+        width: 76px;
+    }
+
+    #eb {
+        background: #ff8905;
+    }
+
+    #db {
+        background: #f25f4c;
+    }
+
+    #copy {
+        height: 108px;
+        background: #3da9fc;
+    }
+
 </style>
